2a5a51a737afdf19a25d5b3f5c3ff2fe98b4e2ce
zahraabs <zahraabbasiun@gmail.com>
2023-02-02 23:06:51 +0330

    s new changes is commited

--- js/index.js
+++ js/index.js
@@ -3,5 +3,21 @@ $(document).ready(function(){
         $(".open--cart").toggleClass("active--cart"); 
         $(".cart").fadeToggle();
     });
+    $(".sale--timeout__counter").startTimer();
 });
+
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
